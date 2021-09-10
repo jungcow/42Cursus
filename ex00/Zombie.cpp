6a209db8ded3bf48f3e1a5172f16458f720bdab6
jungcow <jungwkim@student.42seoul.kr>
2021-09-10 22:43:24 +0900

    Add: ex00, constructor and destructor

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,21 @@
+#include "Zombie.hpp"
+
+void Zombie::announce() {
+  std::cout << "<"
+            << _name
+            << "> BraiiiiiiinnnzzzZ..."
+            << std::endl;
+}
+
+Zombie::Zombie() {}
+
+Zombie::Zombie(std::string name) {
+  _name = name;
+}
+
+Zombie::~Zombie() {
+  std::cout << "<"
+            << _name
+            << "> Destructed"
+            << std::endl;
+}
