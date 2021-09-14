ea6f2e67cf7c33aa194010d0314b0f3d3f9d4891
jungcow <jungwkim@student.42seoul.kr>
2021-09-14 17:29:42 +0900

    add: ex02, same with ex01; repetitive exercise

---
+++ ex02/FragTrap.hpp
@@ -0,0 +1,23 @@
+#ifndef _FRAGTRAP_H__
+#define _FRAGTRAP_H__
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap {
+ public:
+  FragTrap();
+  FragTrap(const std::string name);
+  FragTrap(const FragTrap& other);
+  FragTrap& operator=(const FragTrap& other);
+  ~FragTrap();
+
+  virtual void attack(std::string const& target);
+  virtual void takeDamage(unsigned int amount);
+  virtual void beRepaired(unsigned int amount);
+
+  void highFivesGuys(void);
+};
+
+std::ostream& operator<<(std::ostream& c, const FragTrap& clap);
+
+#endif
