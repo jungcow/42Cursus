1b9e7fd6f81ceb1fab554ba20f331c66aeb451b8
jungcow <jungwkim@student.42seoul.kr>
2021-09-20 11:37:20 +0900

    add: ex00, for learn exception using inheritance of execption class

---
+++ ex00/main.cpp
@@ -0,0 +1,47 @@
+#include "Bureaucrat.hpp"
+
+int main(void) {
+  try {
+    Bureaucrat a(3);
+    Bureaucrat b("b", 148);
+
+    try {
+      std::cout << "\n==============[ Constructor, Destructor Exception Test "
+                   "]============="
+                << std::endl;
+
+      Bureaucrat c(0);
+      Bureaucrat d(151);
+      std::cout << c << std::endl;
+      std::cout << d << std::endl;
+    } catch (const std::exception &e) {
+      std::cerr << e.what() << '\n';
+    }
+    std::cout
+        << "\n============================================================\n"
+        << std::endl;
+    try {
+      std::cout << "\n==============[ increament, decrement Test ]============="
+                << std::endl;
+      Bureaucrat c(1);
+      Bureaucrat d(150);
+
+      std::cout << c << std::endl;
+      std::cout << d << std::endl;
+      c.decreaseGrade();
+      d.increaseGrade();
+      std::cout << c << std::endl;
+      std::cout << d << std::endl;
+    } catch (const std::exception &e) {
+      std::cerr << e.what() << '\n';
+    }
+    std::cout << "\n========================================================\n"
+              << std::endl;
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+  }
+  return 0;
+}
