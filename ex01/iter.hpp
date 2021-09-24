eebc66487cdf944236c73ad0b5aaf311ea6534a5
jungcow <jungwkim@student.42seoul.kr>
2021-09-24 15:16:29 +0900

    refactor: add test case

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,7 +3,24 @@
 
 #include <iostream>
 
-template <typename T> void iter(T *Array, long size, void (*fptr)(T element)) {
+class Awesome {
+public:
+  Awesome(void) : _n(42) { return; }
+  int get(void) const { return this->_n; }
+
+private:
+  int _n;
+};
+std::ostream &operator<<(std::ostream &o, Awesome const &rhs) {
+  o << rhs.get();
+  return o;
+}
+template <typename T> void print(T const &x) {
+  std::cout << x << std::endl;
+  return;
+}
+
+template <typename T> void iter(T *Array, long size, void (*fptr)(T const &)) {
   if (!Array || size <= 0)
     return;
   for (long i = 0; i < size; i++) {
@@ -11,7 +28,7 @@ template <typename T> void iter(T *Array, long size, void (*fptr)(T element)) {
   }
 }
 
-template <typename T> void printArray(T element) {
+template <typename T> void printElement(T element) {
   std::cout << element << std::endl;
 }
 
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,25 +1,22 @@
-#include "Bureaucrat.hpp"
-#include "ShrubberyCreationForm.hpp"
 #include "iter.hpp"
 
 int main(void) {
-  int    i[3] = {1, 2, 3};
-  double d[3] = {1.1, 2.2, 3.3};
+  {
+    int         i[3] = {1, 2, 3};
+    double      d[3] = {1.1, 2.2, 3.3};
+    std::string str[3] = {"hihi", "hello", "world"};
 
-  std::string str[3] = {"hihi", "hello", "world"};
-
-  iter(i, sizeof(i) / sizeof(i[0]), printArray);
-  iter(d, sizeof(d) / sizeof(d[0]), printArray);
-  iter(i, -2, printArray);
-  iter(str, sizeof(str) / sizeof(str[0]), printArray);
+    iter(i, sizeof(i) / sizeof(i[0]), print);
+    iter(d, sizeof(d) / sizeof(d[0]), print);
+    iter(str, sizeof(str) / sizeof(str[0]), print);
+    iter(i, -2, print);
+  }
 
   {
-    ShrubberyCreationForm shru("shru");
-    ShrubberyCreationForm bbery("bbery");
-    ShrubberyCreationForm crea("crea");
-    ShrubberyCreationForm shrubberyCreationForm[3] = {shru, bbery, crea};
-    iter(shrubberyCreationForm,
-         sizeof(shrubberyCreationForm) / sizeof(shrubberyCreationForm[0]),
-         printArray);
+    int     tab[] = {0, 1, 2, 3, 4};
+    Awesome tab2[5];
+    iter(tab, 5, print);
+    iter(tab2, 5, print);
   }
+  return 0;
 }
