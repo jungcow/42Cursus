3f0190879daab455febfd6f5be557a25f513bfb0
jungcow <jungwkim@student.42seoul.kr>
2021-09-26 18:10:28 +0900

    add: ex00, for using find STL algorithm for various containers

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,21 +1,17 @@
 #ifndef _EASYFIND_H__
 #define _EASYFIND_H__
 
-#include <iostream>
 #include <algorithm>
+#include <iostream>
+#include <list>
 #include <vector>
-using namespace std;
 
-template <typename T>
-unsigned int easyfind(vector<T> vi, int n) {
+template <typename T> unsigned int easyfind(T &container, int n) {
 
-	vector<int>::iterator it;
-
-	if (it = find(vi.begin(), vi.end(), n) == vi.end())
-		std::cout << "no\n";
-	std::cout << "yes\n";	
+  if (std::find(container.begin(), container.end(), n) == container.end()) {
+    return (0);
+  }
+  return (1);
 }
-
-
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,9 +1,17 @@
 #include "easyfind.hpp"
 
 int main(void) {
-	int arr[] = {1, 2, 3, 4, 5};
+  int         arr[] = {1, 2, 3, 4, 5};
+  std::vector<int> vi(arr, arr + (sizeof(arr) / sizeof(int)));
+  std::list<int>   li(arr, arr + (sizeof(arr) / sizeof(int)));
+  /*
+  ** vector 컨테이너
+  */
+  easyfind(vi, 6) ? std::cout << "I found it!\n" : std::cout << "Not Found\n";
 
-	vector<int> vi(arr, arr+(sizeof(arr)/sizeof(int)));
-	std::cout << easyfind(vi, 2) << std::endl;
-	return (0);
+  /*
+  ** list 컨테이너
+  */
+  easyfind(li, 1) ? std::cout << "I found it!\n" : std::cout << "Not Found\n";
+  return (0);
 }
